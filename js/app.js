49a6fc51308172ab3d7c4bd63036ffcf18d450ee
Rafailong <rafa.avim@gmail.com>
2012-07-12 21:21:25 -0500

    Adding Add and delete contacts features

--- js/app.js
+++ js/app.js
@@ -12,7 +12,12 @@
 	
 	var Contact = Backbone.Model.extend({
 		defaults: {
-			photo: "img/placeholder.png"
+			photo: "img/placeholder.png",
+			name: "",
+			address: "",
+			tel: "",
+			email: "",
+			type: ""
 		}
 	});
 	
@@ -29,6 +34,19 @@
 			var tmpl = _.template(this.template);
 			$(this.el).html(tmpl(this.model.toJSON()));
 			return this;
+		},
+		
+		events: {
+			"click button.delete": "deleteContact"
+		},
+		
+		deleteContact: function () {
+			var removedType = this.model.get("type").toLowerCase();
+			this.model.destroy();
+			this.remove();
+			if (_.indexOf(directory.getTypes(), removedType) === 1){
+				directory.$el.find("#filter select").children("[value='"+removedType+"']").remove();
+			}
 		}
 	});
 	
@@ -40,6 +58,8 @@
 			this.$el.find("#filter").append(this.createSelect());
 			this.on("change:filterType", this.filterByType, this);
 			this.collection.on("reset", this.render, this);
+			this.collection.on("add", this.renderContact, this);
+			this.collection.on("remove", this.removeContact, this);			
 			this.render();
 		},
 		
@@ -82,7 +102,13 @@
 		},
 		
 		events: {
-			"change #filter select:": "setFilter"
+			"change #filter select:": "setFilter",
+			"click #add": "addContact",
+			"click #showForm": "showForm"
+		},
+		
+		showForm: function () {
+			this.$el.find("#addContact").slideToggle();
 		},
 		
 		setFilter: function (e) {
@@ -104,6 +130,40 @@
 				this.collection.reset(filtered);
 				contactsRouter.navigate("filter/" + filterType);
 			}
+		},
+		
+		addContact: function (e) {
+			e.preventDefault();
+			
+			var newModel = {};
+			$("#addContact").children("input").each(function (i, el) {
+				if ($(el).val() !== ""){
+					newModel[el.id] = $(el).val();
+				}
+			});
+			
+			contacts.push(newModel);
+			
+			if (_.indexOf(this.getTypes(), newModel.type) === 1){
+				this.collection.add(new Contact(newModel));
+				this.$el.find("#filter").find("select").remove().end().append(this.createSelect());
+			}
+			else{
+				this.collection.add(new Contact(newModel));
+			}
+		},
+		
+		removeContact: function (removedModel) {
+			var removed = removedModel.attributes;
+			if (removed.photo === "img/placeholder.png"){
+				delete removed.photo;
+			}
+			
+			_.each(contacts, function (contact){
+				if (_.isEqual(contact, removed)){
+					contacts.splice(_.indexOf(contacts, contact), 1);
+				}
+			})
 		}
 	});
 	
